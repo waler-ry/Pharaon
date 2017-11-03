94c4b072e3c35f91c6e9e08b108f50c3824d998d
waler-ry <antonriazhenov@gmail.com>
2017-11-03 07:32:12 +0200

    add js

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,47 @@
 var next = document.querySelector('.slide-next');
+var prev = document.querySelector('.slide-prev');
 var slide = document.querySelectorAll('.slide-container');
 var i = 0;
 
 next.onclick = function() {
-	slide[i].classList.add('slide-right');
+	slide[i].style.display="none";
+	i++;
+	if(i > 3) {
+		i=0;
+	}	
+	slide[i].style.display="flex";
+};
+
+
+prev.onclick = function() {
+	slide[i].style.display="none";
 	i--;
-	if (i < 0) {
-		i = slide.length;
+	if(i < 0) {
+		i = 3;
 	}
-	slide[i].classList.add('slide-left');
+	slide[i].style.display="flex";
+};
+
+var next1 = document.querySelector('.next');
+var prev1 = document.querySelector('.prev');
+var slide = document.querySelectorAll('.slide-container');
+var i = 0;
+
+next.onclick = function() {
+	slide[i].style.display="none";
+	i++;
+	if(i > 3) {
+		i=0;
+	}	
+	slide[i].style.display="flex";
+};
+
+
+prev.onclick = function() {
+	slide[i].style.display="none";
+	i--;
+	if(i < 0) {
+		i = 3;
+	}
+	slide[i].style.display="flex";
 };
